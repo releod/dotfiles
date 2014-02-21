aa5512f3334f3f3df366987b59430ce95e22bf01
Adam Doeler <adam@adamdoeler.com>
2014-02-21 10:47:43 -0500

    Configures basic install script

--- install.sh
+++ install.sh
@@ -11,53 +11,16 @@ cores() {
 
 ./linker.sh
 
-if [[ ! -d $HOME/.vim/bundle/neobundle.vim ]]; then
-  echo "Installing neobundle..."
-  git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
-  vim +NeoBundleInstall +qall
-fi
-
-if ! type -t chruby > /dev/null 2>&1; then
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "Installing .oh-my-zsh..."
   (
-    git clone git@github.com:postmodern/chruby.git /tmp/chruby-latest
-    cd /tmp/chruby-latest
-    sudo make install
-    rm -rf /tmp/chruby-latest
+    curl -L "https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh" | sh
   )
 fi
 
-if ! type -t ruby-install > /dev/null 2>&1; then
+if ! echo $0 | grep "zsh"; then
+  echo "Swiching shell to zsh..."
   (
-    git clone git@github.com:postmodern/ruby-install.git /tmp/ruby-install-latest
-    sudo make install
-    rm -rf /tmp/ruby-install-latest
+    chsh -s $(which zsh)
   )
 fi
-
-if ! vim --version | grep -q "+ruby"; then
-  echo "Installing Vim 7.4 with Ruby support..."
-  (
-    cd /tmp
-    source /usr/local/share/chruby/chruby.sh 
-    ruby --version
-
-    wget "ftp://ftp.vim.org/pub/vim/unix/vim-$VIM_VERSION.tar.bz2"
-    tar xjf "vim-$VIM_VERSION.tar.bz2"
-    cd vim74
-    ./configure --enable-rubyinterp
-    make -j"$(cores)"
-    sudo make install
-  )
-
-  # Refresh links
-  hash -r
-fi
-
-echo "Compiling commandt..."
-(
-  cd $HOME/.vim/bundle/Command-T/ruby/command-t
-  source /usr/local/share/chruby/chruby.sh 
-  ruby extconf.rb
-  make clean
-  make
-)
